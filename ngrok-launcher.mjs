c4549ec50f3cfeb9532e4d58261aec82bb620bff
m7kor <m7kor@users.noreply.github.com>
2026-09-12 09:52:09 +0300

    chore: remove tunnel/ngrok files

--- ngrok-launcher.mjs
+++
@@ -1,9 +0,0 @@
-import { spawn } from 'node:child_process';
-
-const ng = spawn('C:\\Project\\discord-yt-streamer\\ngrok.exe', ['http', '3334'], {
-  stdio: ['ignore', 'pipe', 'pipe'],
-});
-
-ng.stdout.on('data', (d) => process.stdout.write(d));
-ng.stderr.on('data', (d) => process.stderr.write(d));
-ng.on('exit', (code) => process.exit(code ?? 1));
