19826e62ff857620127fe55692f81ec73b834afa
m7kor <m7kor@users.noreply.github.com>
2026-09-12 23:48:25 +0300

    chore: remove dist from repo and fix gitignore encoding

--- dist/commands/resume.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { resume } from '../services/player/index.js';
-import { executePlayCommand } from './play-command.js';
-export const data = new SlashCommandBuilder()
-    .setName('كمل')
-    .setDescription('استكمال اخر مقطع تم تشغيله');
-export async function execute(interaction) {
-    await executePlayCommand(interaction, resume, 'كمل');
-}
