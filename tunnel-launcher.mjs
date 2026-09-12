10a39fe3317a0ed63e0c2be3749440148c84c2d6
m7kor <m7kor@users.noreply.github.com>
2026-09-12 09:30:45 +0300

    chore: gitignore cloudflared + tunnel files

--- tunnel-launcher.mjs
+++
@@ -1,9 +0,0 @@
-import { spawn } from 'node:child_process';
-
-const cf = spawn('C:\\Project\\discord-yt-streamer\\cloudflared.exe', ['tunnel', '--url', 'http://localhost:3334'], {
-  stdio: ['ignore', 'pipe', 'pipe'],
-});
-
-cf.stdout.on('data', (d) => process.stdout.write(d));
-cf.stderr.on('data', (d) => process.stderr.write(d));
-cf.on('exit', (code) => process.exit(code ?? 1));
--- tunnel.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Project\discord-yt-streamer\cloudflared.exe" tunnel --url http://localhost:3334
