8858bb490767ac24062d6658f4d63cfc165f10e9
m7kor <m7kor@users.noreply.github.com>
2026-09-12 23:52:47 +0300

    chore: correctly exclude dist in gitignore

--- dist/commands/latest.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { playLatest } from '../services/player/index.js';
-import { executePlayCommand } from './play-command.js';
-export const data = new SlashCommandBuilder()
-    .setName('اخر_مقطع')
-    .setDescription('تشغيل احدث مقطع من القناة');
-export async function execute(interaction) {
-    await executePlayCommand(interaction, playLatest, 'اخر_مقطع');
-}
